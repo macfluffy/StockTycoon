440da9514dc1528683b797e0cecf9e8df90a3d6f
macfluffy <shadowimitation@hotmail.com>
2025-08-25 21:30:49 +1000

    feat: Tests the stock value and rarity creation when initialising the stock object.

--- tests.py
+++ tests.py
@@ -5,30 +5,37 @@ def testPassed():
     print("Test Passed!")
 
 def testStockName(stock, testCondition):
-    print("Testing stock name...")
+    print("Comparing the stock names.....")
     assert stock.getBrandName() == testCondition, "The stock's brand name does not match!"
     testPassed()
 
 def testStockType(stock, testCondition):
-    print("Testing stock brand type")
+    print("Comparing the stock brand types.....")
     assert stock.getBrandType() == testCondition, "The stock's brand type does not match!"
     testPassed()
+
+def testStockRarity(stock, testCondition):
+    print("Comparing the stock rarities.....")
+    assert stock.getRarity() == testCondition, "The stock's rarity does not match!"
+    testPassed()
+
+def testStockValue(stock, testCondition):
+    print("Comparing the stock values.....")
+    assert stock.getValue() == testCondition, "The stock's value is different!"
+    testPassed()
+
+def testStock(stock, brandName, brandType, rarity, value):
+    print("Testing Stock was created correctly.....")
+    testStockName(stock, brandName)
+    testStockType(stock, brandType)
+    testStockRarity(stock, rarity)
+    testStockValue(stock, value)
     
 if __name__ == '__main__':
     Coke = stockTyGameEngine.Stock(brandName = "Coca-Cola", brandType = "Food & Beverage", rarity = "Common", value = 3000)
-    testStockName(Coke, "Coca-Cola")
-    testStockType(Coke, "Food & Beverage")
-
-    print(Coke.getRarity())
-    print(Coke.setRarity("Rare"))
-    print(Coke.getRarity())
-
-    Pepsi = stockTyGameEngine.Stock(brandName = "Pepsi", brandType = "Food & Beverage", rarity = "Uncommon", value = 3000)
-    print(Pepsi.getRarity())
-    print(Pepsi.setRarity("Legendary"))
-    print(Pepsi.getRarity())
-
-    Solo = stockTyGameEngine.Stock(brandName = "Solo", brandType = "Food & Beverage", rarity = "Rare", value = 4500)
+    testStock(Coke, brandName = "Coca-Cola", brandType = "Food & Beverage", rarity = "Common", value = 3000)
+    '''Pepsi = stockTyGameEngine.Stock(brandName = "Pepsi", brandType = "Food & Beverage", rarity = "Uncommon", value = 3000)
+    Solo = stockTyGameEngine.Stock(brandName = "Solo", brandType = "Food & Beverage", rarity = "Rare", value = 4500)'''
 
 '''    pack1 = stockTyGameEngine.Pack([Coke, Pepsi, Solo], 1, 2)
     pack1.displayPackContents()
@@ -38,7 +45,7 @@ if __name__ == '__main__':
     print(pack1.getStock(0))
     print("        ")
     print("        ")
-
+    
     player1 = stockTyGameEngine.Player("Josh", 1)
     portfolio1 = stockTyGameEngine.Portfolio(player1.getPlayerName())
     portfolio1.addStock(Coke)
